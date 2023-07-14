d8b4e566b2988b943a885ca238853275b969efb6
Devin Kenney <devin.kenney111@gmail.com>
2023-07-14 11:37:12 -0400

    Week 8 Day 4 Complete

--- unit_2/react_state_store/src/App.js
+++ unit_2/react_state_store/src/App.js
@@ -3,39 +3,20 @@ import './App.css';
 import { useState } from 'react'
 import ProductList from './components/ProductList';
 import products from './data';
+import Cart from './components/Cart';
 
 function App() {
 
   const [cart, setCart] = useState([])
-  const [submission, setSubmission] = useState()
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    setCart([...cart, submission])
-  }
-
-  const handleChange = (event) => {
-    setSubmission(event.target.value)
-  }
 
   return (
     <div className="App">
       <h1> Hi There! </h1>
+      <div className="container">
+        <ProductList products={products} setCart={setCart} cart={cart}/>
 
-      <ProductList product={products[0]}/>
-
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} />
-        <input type="submit" />
-      </form>
-
-      <ul>
-        {cart.map((item, index) => {
-          return (
-            <li key={index}>{item}</li>
-          )
-        })}
-      </ul>
+        <Cart cart={cart} />
+      </div>
     </div>
   );
 }
