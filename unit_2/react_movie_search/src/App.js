cf38b4b0b48f9f05416527b76e169eb91bceeb0c
Devin Kenney <devin.kenney111@gmail.com>
2023-07-19 11:25:16 -0400

    Week 9 Day 3 Part 2

--- unit_2/react_movie_search/src/App.js
+++ unit_2/react_movie_search/src/App.js
@@ -2,28 +2,44 @@ import logo from './logo.svg';
 import './App.css';
 import MovieDisplay from './components/MovieDisplay';
 import Form from './components/Form';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
-  const apiKey="40dc040"
+  const movieList = [
+    "Donnie Darko",
+    "Home Alone",
+    "Black Panther",
+    "The Dark Knight",
+    "Spider-Man"
+  ]
+
+  const apiKey = "40dc040"
 
   const [movie, setMovie] = useState(null)
 
   const getMovie = async (searchTerm) => {
-    const response = await fetch(
-      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
-    )
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      )
 
-    const data = await response.json()
+      const data = await response.json()
 
-    setMovie(data)
+      setMovie(data)
+    } catch (error) {
+      console.error(error)
+    }
   }
+
+  useEffect(() => {
+    getMovie(movieList[Math.floor(Math.random() * movieList.length)]);
+  }, []);
 
   return (
     <div className="App">
       <Form moviesearch={getMovie} />
-      <MovieDisplay />
+      <MovieDisplay movie={movie} />
     </div>
   );
 }
