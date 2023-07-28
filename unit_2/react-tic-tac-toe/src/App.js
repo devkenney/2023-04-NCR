dcdf18e8f220933a4a8f4d0e4c444a6e2db6b8de
Devin Kenney <devin.kenney111@gmail.com>
2023-07-28 10:42:48 -0400

    Week 10 day 2 stuff (modal)

--- unit_2/react-tic-tac-toe/src/App.js
+++ unit_2/react-tic-tac-toe/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Header from './components/Header';
 import Player from './components/Player';
 import Board from './components/Board';
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer, useState } from 'react';
 import Counter from './components/Counter';
 import CounterContext from './context';
 import IsEven from './components/IsEven'
 import ChildExample from './components/ChildExample';
+import Modal from './components/Modal';
 
 function App() {
+
+  const [modalState, setModalState] = useState("disabled")
 
   // The following is the reducer function which we will be using inside of our useReducer hook. This is the function that will be run when changing state
   const reducer = (state, action) => {
@@ -24,6 +27,8 @@ function App() {
         return state
     }
   }
+
+
 
   const initialState = 0
 
@@ -45,6 +50,12 @@ function App() {
       <IsEven number={3} />
 
       <ChildExample><h1>Hello World!</h1></ChildExample>
+
+      <button onClick={() => {
+        setModalState("enabled")
+      }}>Open Modal!</button>
+
+      <Modal modalState={modalState} setModalState={setModalState} />
 
     </div>
   );
--- unit_2/react-tic-tac-toe/src/components/IsEven.js
+++ unit_2/react-tic-tac-toe/src/components/IsEven.js
@@ -8,17 +8,17 @@
 // }
 
 // Single Return Statement, If assigns a value to a variable
-// export default function IsEven(props) {
-//   let result
+export default function IsEven(props) {
+  let result
 
-//   if (props.number % 2 === 0) {
-//     result = <h1>It is even!</h1>
-//   } else {
-//     result = <h1>It is odd!</h1>
-//   }
+  if (props.number % 2 === 0) {
+    result = <h1>It is even!</h1>
+  } else {
+    result = <h1>It is odd!</h1>
+  }
 
-//   return result;
-// }
+  return result;
+}
 
 // Returning a Ternary Operator
 // export default function IsEven(props) {
@@ -42,6 +42,6 @@
 //   return condition ? ifTrue() : ifFalse()
 // }
 
-export default function Modal(props) {
-  <div className={props.visible ? "active" : ""}>{props.children}</div>
-}
+// export default function Modal(props) {
+//   <div className={props.visible ? "active" : ""}>{props.children}</div>
+// }
