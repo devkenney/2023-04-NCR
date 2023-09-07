a4664fa3577ee4a1eef060dfacc5ea5846640873
Devin Kenney <devin.kenney111@gmail.com>
2023-09-07 13:14:39 -0400

    Week 15 Day 4 Part 2

--- unit_3/mern-infra/src/components/NavBar.jsx
+++ unit_3/mern-infra/src/components/NavBar.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as userService from '../utilities/users-service';
 
-export default function NavBar(props) {
+export default function NavBar({ user, setUser }) {
+  function handleLogOut() {
+    userService.logOut();
+    setUser(null);
+  }
+
   return (
     <nav>
-        <Link to="/orders"> Order History </Link>
-        <Link to="/orders/new"> New Order</Link>
-        <span>Welcome, { props.user.name }</span>
+      <Link to="/orders">Order History</Link>&nbsp;&nbsp;
+      <Link to="/orders/new">New Order</Link>
+      &nbsp;&nbsp;<span>Welcome, {user.name}</span>
+      &nbsp;&nbsp;<Link to="" onClick={handleLogOut}>Log Out</Link>
     </nav>
   )
 }
--- unit_3/mern-infra/src/utilities/users-service.js
+++ unit_3/mern-infra/src/utilities/users-service.js
@@ -1,13 +1,5 @@
 import * as usersAPI from './users-api'
 
-export async function signUp(userData) {
-  const token = await usersAPI.signUp(userData);
-
-  localStorage.setItem('token', token);
-
-  return getUser();
-}
-
 export function getToken() {
   const token = localStorage.getItem('token');
   if (!token) return null;
@@ -26,4 +18,29 @@ export function getUser() {
   const token = getToken();
 
   return token ? JSON.parse(atob(token.split('.')[1])).user : null
+}
+
+export async function signUp(userData) {
+  const token = await usersAPI.signUp(userData);
+
+  localStorage.setItem('token', token);
+
+  return getUser();
+}
+
+export async function login(credentials) {
+  const token = await usersAPI.login(credentials)
+
+  localStorage.setItem('token', token);
+
+  return getUser()
+}
+
+export async function checkToken() {
+  return usersAPI.checkToken()
+    .then(dateStr => new Date(dateStr));
+}
+
+export function logOut() {
+  localStorage.removeItem('token');
 }
